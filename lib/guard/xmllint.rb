00aeaffa041b8ad5f2aab7dbf739921f06857cb2
Larry Gilbert <larry@l2g.to>
2013-08-14 22:39:24 -0700

    Add initializer, run_all/run_on_changes, :files option

--- lib/guard/xmllint.rb
+++ lib/guard/xmllint.rb
@@ -6,6 +6,25 @@ require 'guard/xmllint/version'
 module Guard
   class XmlLint < Guard
 
+    def initialize(watchers = [], options = {})
+      super
+      @options = {
+        files: '**/*.xml'
+      }.update(options)
+    end
+
+    def run_all
+      UI.debug("XML-linting all files matching #{options[:files]}")
+      run_on_changes(Dir.glob(options[:files]))
+    end
+
+    def run_on_changes(paths)
+      paths.each do |path|
+        UI.debug("XML-linting #{path}")
+        lint_pick(path) if File.file?(path)
+      end
+    end
+
     # Returns this guard's name, in accordance with Guard's plugin API
     #
     # @api public
@@ -15,6 +34,12 @@ module Guard
 
     def to_s
       'XmlLint'
+    end
+
+    private
+
+    def lint_pick(path)
+      UI.debug("Picking at #{path} (well, I would be!)")
     end
   end
 end
